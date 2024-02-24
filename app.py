0bfc433573e13a1801921e1e8e0070cfe584972a
kylle <kylle.harripersad@gmail.com>
2024-02-24 02:31:17 -0400

    apparently the app.py from last lab somehow got   linked here, so here is the real lab3

--- app.py
+++ app.py
@@ -1,31 +1,69 @@
-from fastapi import FastAPI, HTTPException
+import uuid
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, HTTPException,Request,Response,status
 app=FastAPI()
 
-data=[]
 
-@app.post("/person")
-async def add_person(result: dict):
-    if result["name"] and result["occupation"] and result[ "address"]:
-        data.append(result)
-        return {"sucess": True,"result":result} 
-            
-            
+tanks=[]
+
+# Create a new dictionary
+
+@app.post("/tank")
+async def display (tank:dict,response:Response):
+   output=[]
+   new_uuid = str(uuid.uuid4())
+
+   tank["id"] = new_uuid
+   tank["location"]
+   tank["lat"]
+   tank["lat"]
+   tanks.append(tank)
+   output.append(tank)
+   response.status_code = 201  # Set status code to 201 Created
+   return output
+   
+
+@app.get("/tank")
+async def show_data():
+   return tanks
+
+# Define a function to find a tank by its id
+def find_tank_by_id(id):
+    for tank in tanks:
+        if tank["id"] == id:
+            return tank
+    return None
+
+# Endpoint to retrieve data for a specific tank by its id
+@app.get("/tank/{id}")
+async def show_tank(id: str):
+    tank = find_tank_by_id(id)
+    if tank:
+        return tank
     else:
-        result={"error_message":"invalid request"}
-        return {"sucess": False,"result":result} 
+        raise HTTPException(status_code=404, detail="Tank not found")
 
-@app.get("/person")
-async def get_data():
-    output=[]
-    for person in data:
-        formatted_person = {
-            "name": person["name"],
-            "occupation": person["occupation"],
-            "address": person["address"]
-        }
-        output.append(formatted_person)
-    return output
+@app.patch("/tank/{id}")
+async def partial_edit(id:str,request:Request,response:Response):
+    edit= await request.json()
+    for i,tank in enumerate(tanks):
+        if tank["id"] == id:
+            tanks[i]={**tank, **edit}
+            tanks[i]["id"]=id # repastes old id
+            response.status_code = 206  # indicates partial change
+            return tanks[i]
+    raise HTTPException(status_code=404, detail=f"Tank with ID {id} not found")
+    
+@app.delete("/tank/{id}")
+async def delete_T(id: str, response: Response):
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    for i in range(len(tanks)):
+        if tanks[i]["id"] == id:
+            del tanks[i]
+            response.status_code = 204  # indicatre
+            return()
+    
+    raise HTTPException(status_code=404, detail="Item not found")    
+
+import uvicorn
+uvicorn.run(app, host="127.0.0.1", port=8000)
